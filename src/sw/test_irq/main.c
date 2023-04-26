ed549e864db00d1a5c2fd3c0e92ede317126317f
Sergei Gunstvin <gunstvin@campus.tu-berlin.de>
2023-04-26 16:17:05 +0200

    Add timer_irq for later usage

--- src/sw/test_irq/main.c
+++ src/sw/test_irq/main.c
@@ -3,18 +3,32 @@
 
 void irq_handler(void) {
     fputs("I am interrupt\n", stdout);
+    REG32(RV_TIMER_INTR_STATE0(0)) = 1; // clears the timer interrupt
+	REG32(RV_TIMER_INTR_ENABLE0(0)) = 0;
+}
+
+static void run_timer_irq(int n_cycles) {
+    REG32(RV_TIMER_CTRL(0)) = (1<<RV_TIMER_CTRL_ACTIVE0_LSB);
+    REG32(RV_TIMER_TIMER_V_LOWER0(0)) = 0;
+    REG32(RV_TIMER_TIMER_V_UPPER0(0)) = 0;
+    REG32(RV_TIMER_COMPARE_LOWER0_0(0)) = n_cycles;
+    REG32(RV_TIMER_COMPARE_UPPER0_0(0)) = 0;
 }
 
 int main(void) {
-    int a = 0x800;
-	asm volatile ("csrs mie, %0":: "r" (a));
+	asm volatile ("csrs mie, %0":: "r" (0x800)); // enables external interrupts
+	asm volatile ("csrs mie, %0":: "r" (0x80)); // enables timer interrupts
 	asm volatile ("csrs mstatus, 0x8");
+	
 
     int i = 1234;
     int counter = 0;
     while(counter < 5) {
+        REG32(RV_TIMER_INTR_ENABLE0(0)) = 1;
+        run_timer_irq(5);
     	counter++;
     	printf("I am loop (%i)\n", i++);
     }
+
     return 0;
 }
