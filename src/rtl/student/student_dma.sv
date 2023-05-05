0629a7f0ba08e03428ee5e345ec653c956c3007a
Sergei Gunstvin <gunstvin@campus.tu-berlin.de>
2023-05-05 12:05:26 +0200

    WIP student DMA

---
+++ src/rtl/student/student_dma.sv
@@ -0,0 +1,194 @@
+module student_dma (
+  input logic clk_i,
+  input logic rst_ni,
+
+  input  tlul_pkg::tl_h2d_t tl_i,
+  output tlul_pkg::tl_d2h_t tl_o,
+
+  input  tlul_pkg::tl_d2h_t tl_host_i,
+  output tlul_pkg::tl_h2d_t tl_host_o
+);
+  import student_dma_reg_pkg::*;
+
+  student_dma_reg2hw_t reg2hw;
+  student_dma_hw2reg_t hw2reg;
+
+  student_dma_reg_top reg_top_i (
+    .clk_i,
+    .rst_ni,
+    .tl_i,
+    .tl_o,
+    .reg2hw,
+    .hw2reg,
+    .devmode_i('1)
+  );
+  
+  typedef enum logic [6:0] {
+    IDLE,
+    READ_DESC_SEND,
+    READ_DESC_RECV,
+    MEMSET_WRITING, // ready to try to write next thing
+    MEMSET_WAITING // still trying to send next thing
+  } state_dma_t;
+  
+  state_dma_t current_state;
+  state_dma_t next_state;
+
+  logic [31:0] now_dadr;
+  logic [1:0] status;
+  logic [31:0] src_adr;
+  logic [31:0] dst_adr;
+  logic [31:0] length;
+  logic [31:0] offset;
+  logic cmd;
+  logic still_sending;
+  logic read_desc;
+  logic do_operation;
+  logic set_or_copy;
+  logic desc_response_received;
+  logic write_done;
+
+    
+  always_ff @(posedge clk_i, negedge rst_ni) begin
+    if (~rst_ni) begin
+      current_state <= IDLE;
+    end else begin
+      current_state <= next_state;
+    end
+  end
+  
+  always_comb begin
+  	next_state = current_state;
+    case(current_state)
+    	IDLE: begin
+    		if(read_desc == 1) begin
+    			next_state = READ_DESC_SEND;
+			end
+		end
+		READ_DESC_SEND: begin
+			next_state = READ_DESC_RECV;
+		end
+		READ_DESC_RECV: begin
+    		if(desc_response_received)
+    			next_state = READ_DESC_SEND;
+    		if (do_operation)
+    			next_state = MEMSET_WRITING;
+		end
+		MEMSET_WRITING: begin
+			next_state = length > 0 ? MEMSET_WAITING : IDLE;
+		end
+		MEMSET_WAITING: begin
+			if (write_done) next_state = MEMSET_WRITING;
+			if (write_done && (length == 0)) next_state = IDLE;
+		end
+		default: begin
+			$display("dma fsm default case");
+			next_state = current_state;
+		end
+    endcase
+	if (cmd != '0) next_state = IDLE;
+  end
+  
+  assign hw2reg.status.d = status;
+  assign hw2reg.length.d = length;
+  assign hw2reg.src_adr.d = src_adr;
+  assign hw2reg.dst_adr.d = dst_adr;
+  always_ff @(posedge clk_i, negedge rst_ni) begin
+    if (~rst_ni) begin
+      status <= IDLE;
+      src_adr <= '0;
+      dst_adr <= '0;
+      cmd <= '0;
+      now_dadr <= '0;
+      length <= '0;
+      read_desc <= 0;
+    end else begin
+    	  read_desc <= 0;
+      	  if (reg2hw.cmd.qe) cmd <= reg2hw.cmd.q;
+      	  
+      if (current_state == IDLE) begin
+      	  if(reg2hw.now_dadr.qe == 1) begin
+      	  	  now_dadr <= reg2hw.now_dadr.q;
+      	  	  read_desc <= reg2hw.now_dadr.qe;
+      	  end
+      end
+    end
+  end
+  
+  // TL-UL host handling
+  always_ff @(posedge clk_i, negedge rst_ni) begin
+    if (~rst_ni) begin
+    	still_sending <= 0;
+    	offset <= '0;
+    	tl_host_o <= '{a_opcode: tlul_pkg::PutFullData, default: '0};
+
+    	do_operation <= '0;
+    	set_or_copy <= '0;
+    	desc_response_received <= '0;
+    	write_done <= '0;
+    end else begin
+		do_operation <= '0;
+		tl_host_o.a_valid <= '0;
+		desc_response_received <= '0;
+		write_done <= '0;
+    	case(current_state)
+			READ_DESC_SEND: begin
+				if (tl_host_i.a_ready) begin
+					tl_host_o <= '{a_opcode: tlul_pkg::Get, default: '0};
+					tl_host_o.a_valid <= '1;
+					tl_host_o.a_size <= 2; // Request size (requested size is 2^a_size, thus 0 = byte, 1 = 16b, 2 = 32b, 3 = 64b, etc)
+					tl_host_o.a_source <= 8'h30000000;
+					tl_host_o.a_address <= now_dadr + offset;
+					tl_host_o.a_mask <= '1; // mask not needed here
+					tl_host_o.a_data <= src_adr;
+					tl_host_o.d_ready <= '1;
+					
+    			end
+			end
+			READ_DESC_RECV: begin
+    			if(tl_host_i.d_valid == 1) begin
+					desc_response_received <= '1;
+    				if (offset == 0)
+    					set_or_copy <= tl_host_i.d_data;
+    				if (offset == 4)
+    					length <= tl_host_i.d_data;
+    				if (offset == 8)
+    					src_adr <= tl_host_i.d_data;
+    				if (offset == 12) begin
+    					dst_adr <= tl_host_i.d_data;
+						do_operation <= '1;
+						offset <= '0;
+					end
+    				offset <= offset + 4;
+				end
+			end
+			
+    		MEMSET_WRITING: begin
+				if (tl_host_i.a_ready) begin
+					tl_host_o <= '{a_opcode: tlul_pkg::PutFullData, default: '0};
+					tl_host_o.a_valid <= '1;
+					tl_host_o.a_size <= 2; // Request size (requested size is 2^a_size, thus 0 = byte, 1 = 16b, 2 = 32b, 3 = 64b, etc)
+					tl_host_o.a_source <= 8'h30000000;
+					tl_host_o.a_address <= dst_adr;
+					tl_host_o.a_mask <= '1; // mask not needed here
+					tl_host_o.a_data <= src_adr;
+					tl_host_o.d_ready <= '1;
+				end
+    		end
+    		MEMSET_WAITING: begin
+    			if(tl_host_i.d_valid == 1) begin
+    				write_done <= 1;
+    				length <= length - 4;
+    				dst_adr <= dst_adr + 4;
+    			end
+    		end
+    		default: begin //same as IDLE
+    			tl_host_o <= '{a_opcode: tlul_pkg::PutFullData, default: '0};
+    			tl_host_o.d_ready <= '1;
+    			offset <= '0;
+    		end
+    	endcase
+    end
+  end
+
+endmodule
--- src/sw/dma/main.c
+++ src/sw/dma/main.c
@@ -1,7 +1,52 @@
 #include <stdio.h>
 #include "rvlab.h"
 
+struct descriptor {
+  unsigned int set_or_copy; // memset is 0, memcpy is 1
+  unsigned int length;
+  unsigned int src_adr;
+  unsigned int dst_adr;
+};
+
+/*void memset_hard(void * ptr, int c, unsigned int length) { //size_t?
+	struct descriptor d;
+    d.set_or_copy = 1;
+    d.length = length;
+    d.src_adr = ptr;
+    d.dst_adr = value;
+	REG32(STUDENT_DMA_NOW_DADR(0)) = &d;
+	while(STUDENT_DMA_STATUS(0) != 0) {} // should wait until status is non-zero, hopefully no race condition
+}
+
+void memset_soft(void * ptr, int value, unsigned int length) { //size_t?
+	for (int i = 0; i < length; i++) {
+		*(ptr+i) = value;
+	}
+}
+
+void memcpy_hard(void *dest, void *src, unsigned int length);
+
+void memcpy_soft(void *dest, void *src, unsigned int length);
+*/
 int main(void) {
-    printf("Hello!\n");
+    struct descriptor d;
+    d.set_or_copy = 1;
+    d.length = 16;
+    d.src_adr = 55;
+    d.dst_adr = 0x80000000;
+    printf("%x\n", &d);
+    printf("descriptor: set_or_copy %u, length %u, src_adr %u, dst_adr %u \n", d.set_or_copy, d.length, d.src_adr, d.dst_adr);
+	REG32(STUDENT_DMA_NOW_DADR(0)) = &d;
+	volatile int b = REG32(STUDENT_DMA_NOW_DADR(0));
+    //printf("Hello %x!\n", b);
+    
+    //outputs 0 while hw2reg outputs correct value
+    volatile unsigned int length = REG32(STUDENT_DMA_LENGTH(0));
+    volatile unsigned int src_adr = REG32(STUDENT_DMA_SRC_ADR(0));
+    volatile unsigned int dst_adr = REG32(STUDENT_DMA_DST_ADR(0));
+    printf("reg values: length %u, src_adr %u, dst_adr %u \n", length, src_adr, dst_adr);
+    unsigned int * a = 0x80000000;
+    printf("%u", *a);
+
     return 0;
 }
