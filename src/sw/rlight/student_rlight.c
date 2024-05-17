9183cb9c0ab314f150f68478335b5e80a5b0e8a7
Risc V Lab Account <rvlab01@msclab.msc.tu-berlin.de>
2024-05-17 11:24:00 +0200

    test function of regD tested

--- src/sw/rlight/student_rlight.c
+++ src/sw/rlight/student_rlight.c
@@ -30,10 +30,8 @@ int student_rlight_test(void) {
         retval = 1;
     printf("set RegC: %i (%s)\n", readword, correct?"correct":"WRONG");
 
-    correct = (readword = REG32(STUDENT_RLIGHT_REGD(0))) == 0x3C;
-    if(!correct)
-        retval = 1;
-    printf("read RegD: %i (%s)\n", readword, correct?"correct":"WRONG");
+    readword = REG32(STUDENT_RLIGHT_REGD(0));
+    printf("actual LED status is: %i \n", readword);
 
     return retval;
 }
