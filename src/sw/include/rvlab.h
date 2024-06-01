e41f26845785f458cff218f74af475c971c99d0c
Risc V Lab Account <rvlab01@msclab.msc.tu-berlin.de>
2024-06-01 19:14:21 +0200

    added base address of irq_CTRL and changed base address of student_rlight

--- src/sw/include/rvlab.h
+++ src/sw/include/rvlab.h
@@ -25,7 +25,10 @@
 // Add includes for additional register definition headers
 // and define corresponding _BASE_ADDR values here.
 
+#include "reggen/irq_ctrl.h"
+#define IRQ_CTRL0_BASE_ADDR 0x10000000
+
 #include "reggen/student_rlight.h"
-#define STUDENT_RLIGHT0_BASE_ADDR 0x10000000
+#define STUDENT_RLIGHT0_BASE_ADDR 0x10100000
 
 #endif // _RVLAB_H
