98b5669239b976db18a85de93de8b17634c35c5a
Hejie <hejie.zhu@tu-braunschweig.de>
2024-06-02 00:41:07 +0200

    irq ctrl in rtl file finished, but not tested yet

--- src/rtl/student/student_irq_ctrl.sv
+++ src/rtl/student/student_irq_ctrl.sv
@@ -4,19 +4,15 @@ module irq_controller #(
     input logic clk_i,
     input logic rst_ni,
 
-    input logic [N-1:0] irq_i,    					// irq inputs
-	output logic irq_en_o 						// IRQ enable output
+    input logic [N-1:0] irq_i,    	// irq inputs
+	output logic irq_en_o, 			// IRQ enable output
 
 	output tlul_pkg::tl_d2h_t tl_o,  //slave output (this module's response)
   	input  tlul_pkg::tl_h2d_t tl_i,  //master input (incoming request)
 );
-
-    //logic [N-1:0] mask;
-	//logic [N-1:0] former_status;
-    logic [$clog2(N)-1:0] irq__no,  // current IRQ number
+    logic [$clog2(N+1)-1:0] irq__no,  // current IRQ number with one extra bit for 'N'
     logic [N-1:0] irq_masked;
-    logic [N-1:0] irq_prio;
-
+	logic found;
 
 	import irq_ctrl_reg_pkg::*;
 
@@ -34,7 +30,7 @@ module irq_controller #(
 	);
 
     // Mask and status register logic
-    always_ff @(posedge clk, negedge reset) begin
+    always_ff @(posedge clk_i, negedge rst_ni) begin
         if (~reset) begin
             hw2reg.mask.d <= '0;
 			hw2reg.status.d <= '0;
@@ -44,17 +40,28 @@ module irq_controller #(
         end
     end
 
-    // IRQ masking logic assign is a combinatorial logic statement
-    assign irq_masked = irq_i & reg2hw.mask.q;
+	//assign irq_input based on test register
+	assign irq_input = (reg2hw.test.q = 1'b1) ? reg2hw.test_irq.q : irq_i;
 
-    // Priority encoder
+    // IRQ masking logic assign is a combinatorial logic statement
+    assign irq_masked = irq_input & reg2hw.mask.q;
+
+    // Priority and irq_no encoder
     always_comb begin
+		found = 0;
         for (int i = 0; i < N; i++) begin
             if (irq_masked[i]) begin
                 hw2reg.irq_no.d = i;
+				found = 1;
                 break;
             end
         end
+		if (!found) begin
+			hw2reg.irq_no.d = N;
+		end
     end
+
+	// IRQ enable logic
+	assign irq_en_o = (|irq_masked) & reg2hw.all_en.q;
 
 endmodule
