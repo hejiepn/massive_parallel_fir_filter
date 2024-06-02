fd6f8b0e7ec1fa811d331a5062792c8b92cc6f14
Hejie <hejie.zhu@tu-braunschweig.de>
2024-06-02 14:49:22 +0200

    inline keyword syntax corrected

--- src/sw/irq_ctrl/student_irq_ctrl.h
+++ src/sw/irq_ctrl/student_irq_ctrl.h
@@ -10,59 +10,59 @@
 
 //write functions
 
-extern __inline__ void irq_ctrl_disable(void) {
+extern inline void irq_ctrl_disable(void) {
     REG32(IRQ_CTRL_ALL_EN(0)) = 0;
 }
 
-extern __inline__ void irq_ctrl_enable(void) {
+extern inline void irq_ctrl_enable(void) {
     REG32(IRQ_CTRL_ALL_EN(0)) = 1;
 }
 
-extern __inline__ void irq_ctrl_set_mask(uint32_t mask_value) {
+extern inline void irq_ctrl_set_mask(uint32_t mask_value) {
     REG32(IRQ_CTRL_MASK(0)) = mask_value;
 }
 
-extern __inline__ void irq_ctrl_set_mask_set(unsigned int n) {
+extern inline void irq_ctrl_set_mask_set(unsigned int n) {
     REG32(IRQ_CTRL_MASK_SET(0)) = 1 << n;
 }
 
-extern __inline__ void irq_ctrl_set_mask_clear(unsigned int n) {
+extern inline void irq_ctrl_set_mask_clear(unsigned int n) {
     REG32(IRQ_CTRL_MASK_CLR(0)) = 1 << n;
 }
 
-extern __inline__ void irq_ctrl_set_test_mode(bool enable) {
+extern inline void irq_ctrl_set_test_mode(bool enable) {
     REG32(IRQ_CTRL_TEST(0)) = enable ? 1 : 0;
 }
 
-extern __inline__ void irq_ctrl_set_test_irq(uint32_t test_irq_value) {
+extern inline void irq_ctrl_set_test_irq(uint32_t test_irq_value) {
     REG32(IRQ_CTRL_TEST_IRQ(0)) = test_irq_value;
 }
 
 //read functions
 
-extern __inline__ unsigned int get_irq_ctrl_all_en(void) {
+extern inline unsigned int get_irq_ctrl_all_en(void) {
 	unsigned int read_all_en;
 	uint32_t readReg_all_en;
 
-	readReg_all_en =  REG32(IRQ_CTRL_MASK_ALL(0));
+	readReg_all_en =  REG32(IRQ_CTRL_ALL_EN(0));
 	read_all_en = readReg_all_en & IRQ_CTRL_ALL_EN_MASK;
 
 	return read_all_en;
 }
 
-extern __inline__ uint32_t get_irq_ctrl_mask(void) {
+extern inline uint32_t get_irq_ctrl_mask(void) {
 	return REG32(IRQ_CTRL_MASK(0));
 }
 
-extern __inline__ uint32_t get_irq_ctrl_status(void) {
+extern inline uint32_t get_irq_ctrl_status(void) {
     return REG32(IRQ_CTRL_STATUS(0));
 }
 
-extern __inline__ uint32_t get_irq_ctrl_irq_no(void) {
+extern inline uint32_t get_irq_ctrl_irq_no(void) {
     return REG32(IRQ_CTRL_IRQ_NO(0));
 }
 
-extern __inline__ unsigned int get_irq_ctrl_test(void) {
+extern inline unsigned int get_irq_ctrl_test(void) {
 	unsigned int read_test;
 	uint32_t readReg_test;
 
@@ -72,7 +72,7 @@ extern __inline__ unsigned int get_irq_ctrl_test(void) {
 	return read_test;
 }
 
-extern __inline__ uint32_t get_irq_ctrl_test_irq(void) {
+extern inline uint32_t get_irq_ctrl_test_irq(void) {
 	return REG32(IRQ_CTRL_TEST_IRQ(0));
 }
 
