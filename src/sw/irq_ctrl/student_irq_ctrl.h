54b8465501527dcc3c6115d1f1fb2ecd00716cf3
Hejie <hejie.zhu@tu-braunschweig.de>
2024-06-02 14:56:47 +0200

    changed extern keyword to static, suggestion to solve the problem of multiple definition, getting this error from terminal

--- src/sw/irq_ctrl/student_irq_ctrl.h
+++ src/sw/irq_ctrl/student_irq_ctrl.h
@@ -10,37 +10,37 @@
 
 //write functions
 
-extern inline void irq_ctrl_disable(void) {
+static inline void irq_ctrl_disable(void) {
     REG32(IRQ_CTRL_ALL_EN(0)) = 0;
 }
 
-extern inline void irq_ctrl_enable(void) {
+static inline void irq_ctrl_enable(void) {
     REG32(IRQ_CTRL_ALL_EN(0)) = 1;
 }
 
-extern inline void irq_ctrl_set_mask(uint32_t mask_value) {
+static inline void irq_ctrl_set_mask(uint32_t mask_value) {
     REG32(IRQ_CTRL_MASK(0)) = mask_value;
 }
 
-extern inline void irq_ctrl_set_mask_set(unsigned int n) {
+static inline void irq_ctrl_set_mask_set(unsigned int n) {
     REG32(IRQ_CTRL_MASK_SET(0)) = 1 << n;
 }
 
-extern inline void irq_ctrl_set_mask_clear(unsigned int n) {
+static inline void irq_ctrl_set_mask_clear(unsigned int n) {
     REG32(IRQ_CTRL_MASK_CLR(0)) = 1 << n;
 }
 
-extern inline void irq_ctrl_set_test_mode(bool enable) {
+static inline void irq_ctrl_set_test_mode(bool enable) {
     REG32(IRQ_CTRL_TEST(0)) = enable ? 1 : 0;
 }
 
-extern inline void irq_ctrl_set_test_irq(uint32_t test_irq_value) {
+static inline void irq_ctrl_set_test_irq(uint32_t test_irq_value) {
     REG32(IRQ_CTRL_TEST_IRQ(0)) = test_irq_value;
 }
 
 //read functions
 
-extern inline unsigned int get_irq_ctrl_all_en(void) {
+static inline unsigned int get_irq_ctrl_all_en(void) {
 	unsigned int read_all_en;
 	uint32_t readReg_all_en;
 
@@ -50,19 +50,19 @@ extern inline unsigned int get_irq_ctrl_all_en(void) {
 	return read_all_en;
 }
 
-extern inline uint32_t get_irq_ctrl_mask(void) {
+static inline uint32_t get_irq_ctrl_mask(void) {
 	return REG32(IRQ_CTRL_MASK(0));
 }
 
-extern inline uint32_t get_irq_ctrl_status(void) {
+static inline uint32_t get_irq_ctrl_status(void) {
     return REG32(IRQ_CTRL_STATUS(0));
 }
 
-extern inline uint32_t get_irq_ctrl_irq_no(void) {
+static inline uint32_t get_irq_ctrl_irq_no(void) {
     return REG32(IRQ_CTRL_IRQ_NO(0));
 }
 
-extern inline unsigned int get_irq_ctrl_test(void) {
+static inline unsigned int get_irq_ctrl_test(void) {
 	unsigned int read_test;
 	uint32_t readReg_test;
 
@@ -72,7 +72,7 @@ extern inline unsigned int get_irq_ctrl_test(void) {
 	return read_test;
 }
 
-extern inline uint32_t get_irq_ctrl_test_irq(void) {
+static inline uint32_t get_irq_ctrl_test_irq(void) {
 	return REG32(IRQ_CTRL_TEST_IRQ(0));
 }
 
