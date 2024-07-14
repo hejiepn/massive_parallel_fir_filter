00db90be54f0d763181adde930490446fb71f4eb
Risc V Lab Account <rvlab01@msclab.msc.tu-berlin.de>
2024-07-14 23:04:47 +0200

    testbench works now, that fir finishes in 1027 cycles

--- src/fv/student_fir_tb.sv
+++ src/fv/student_fir_tb.sv
@@ -1,9 +1,9 @@
 module student_fir_tb;
 
-  localparam int AddrWidth = 2; // Address width
+  localparam int AddrWidth = 10; // Address width
   localparam int MaxAddr = 2**AddrWidth; // Maximum address
   localparam int DATA_SIZE = 16; // Data size
-  localparam int DEBUGMODE = 1; // activate debugMode when AddrWidth != 10 
+  localparam int DEBUGMODE = 0; // activate debugMode when AddrWidth != 10 
 
   // Clock and reset signals
   logic clk_i;
@@ -52,6 +52,9 @@ module student_fir_tb;
     .tl_o(tl_h2d)
   );
 
+  logic [31:0] clk_count;
+  logic counting;
+
   // Clock generation: 50 MHz clock -> period = 20ns
    always begin
     clk_i = '1;
@@ -79,20 +82,30 @@ module student_fir_tb;
     #40;
 	bus.wait_cycles(20);
 
-	
 	// Apply test stimulus
 	for (i = 0; i < MaxAddr; i = i + 1) begin
-		@(posedge clk_i);
 		sample_in = {8'b0, sin_mem[i]}; // Zero-pad the 8-bit value to 16 bits
 		valid_strobe_in <= 1;
+		counting = 1;
 		@(posedge clk_i);
 		valid_strobe_in <= 0;
 		wait(valid_strobe_out == 1); // Wait for valid_strobe_out to go high
+		counting = 0;
+		$display("Number of clock cycles from valid_strobe_in to valid_strobe_out: %0d", clk_count);
+        clk_count = 0; // Reset counter for next iteration
+		@(posedge clk_i);
 	end
 
     // Finish simulation
     #200;
     $stop;
+  end
+
+  // Clock cycle counter
+  always @(posedge clk_i) begin
+    if (counting) begin
+      clk_count <= clk_count + 1;
+    end
   end
 
   // Monitor to print values
